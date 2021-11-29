44a7413e820875b7022c2fedd9a4b70087ae19c5
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2021-11-29 17:39:05 +0100

    chore: update to storybook 6.4.0

--- helpers/index.js
+++ helpers/index.js
@@ -10,19 +10,18 @@ export async function createDesignTokens(root, cssFiles) {
     const patterns = Array.isArray(cssFiles) ? cssFiles : [cssFiles];
     const designTokensConfig = {
         files: (await Promise.all(
-            patterns
-                .map((pattern) =>
-                    glob(pattern).then((files) =>
-                        Promise.all(
-                            files.map((async (filename) => ({
-                                filename: path.relative(root, filename),
-                                content: (await readFile(filename, 'utf-8'))
-                                    .replace(/\\/g, '\\\\')
-                                    .replace(/\n/g, '\\n'),
-                            })))
-                        )
+            patterns.map((pattern) =>
+                glob(pattern).then((files) =>
+                    Promise.all(
+                        files.map((async (filename) => ({
+                            filename: path.relative(root, filename),
+                            content: (await readFile(filename, 'utf-8'))
+                                .replace(/\\/g, '\\\\')
+                                .replace(/\n/g, '\\n'),
+                        })))
                     )
                 )
+            )
         )).flat(),
         options: {
             hideMatchingHardCodedValues: true,
@@ -58,54 +57,38 @@ export async function createDesignTokensFile(root, cssFiles) {
     return file;
 }
 
-/**
- * Generate a map of modules available in their prebuilt version.
- * @param {string} type
- */
-export function createBuildMap(type) {
-    const modules = {
-        'react': '@chialab/storybook-prebuilt/react',
-        'react-dom': '@chialab/storybook-prebuilt/react',
-        'react-is': '@chialab/storybook-prebuilt/react',
-        '@mdx-js/react': '@chialab/storybook-prebuilt/mdx',
-        '@storybook/manager': '@chialab/storybook-prebuilt/manager',
-        [`@storybook/${type}`]: `@chialab/storybook-prebuilt/${type}`,
-        [`@storybook/${type}/preset.js`]: `@chialab/storybook-prebuilt/${type}/preset.js`,
-        '@storybook/api': '@chialab/storybook-prebuilt/api',
-        '@storybook/addons': '@chialab/storybook-prebuilt/addons',
-        '@storybook/channel-postmessage': '@chialab/storybook-prebuilt/channel-postmessage',
-        '@storybook/channels': '@chialab/storybook-prebuilt/channels',
-        '@storybook/csf': '@chialab/storybook-prebuilt/csf',
-        '@storybook/client-api': '@chialab/storybook-prebuilt/client-api',
-        '@storybook/client-logger': '@chialab/storybook-prebuilt/client-logger',
-        '@storybook/core-events': '@chialab/storybook-prebuilt/core-events',
-        '@storybook/preview-web': '@chialab/storybook-prebuilt/preview-web',
-        '@storybook/components': '@chialab/storybook-prebuilt/components',
-        '@storybook/router': '@chialab/storybook-prebuilt/router',
-        '@storybook/semver': '@chialab/storybook-prebuilt/semver',
-        '@storybook/store': '@chialab/storybook-prebuilt/store',
-        '@storybook/theming': '@chialab/storybook-prebuilt/theming',
-        '@storybook/addon-docs': '@chialab/storybook-prebuilt/docs',
-        '@storybook/addon-docs/blocks': '@chialab/storybook-prebuilt/docs',
-        '@storybook/addon-essentials/register': '@chialab/storybook-prebuilt/essentials/register',
-        '@storybook/addon-essentials': '@chialab/storybook-prebuilt/essentials',
-        '@storybook/addon-storysource/register': '@chialab/storybook-prebuilt/storysource/register',
-        'storybook-design-token': '@chialab/storybook-prebuilt/storybook-design-token',
-        'storybook-design-token/dist/doc-blocks.js': '@chialab/storybook-prebuilt/storybook-design-token',
-        'storybook-design-token/register': '@chialab/storybook-prebuilt/storybook-design-token/register',
-    };
+export const managerEntries = ['@storybook/manager'];
 
-    const resolutions = [];
-    if (type === 'web-components') {
-        resolutions.push('lit-html');
-    }
-    if (type === 'dna') {
-        resolutions.push('lit-html', '@chialab/dna');
-    }
-
-    return {
-        manager: '@storybook/manager',
-        modules,
-        resolutions,
-    };
-}
+export const aliasMap = {
+    'react': '@chialab/storybook-prebuilt/react',
+    'react-dom': '@chialab/storybook-prebuilt/react',
+    'react-is': '@chialab/storybook-prebuilt/react',
+    '@mdx-js/react': '@chialab/storybook-prebuilt/mdx',
+    '@storybook/manager': '@chialab/storybook-prebuilt/manager',
+    '@storybook/web-components': '@chialab/storybook-prebuilt/web-components',
+    '@storybook/web-components/preset.js': '@chialab/storybook-prebuilt/web-components/preset.js',
+    '@storybook/dna': '@chialab/storybook-prebuilt/dna',
+    '@storybook/dna/preset.js': '@chialab/storybook-prebuilt/dna/preset.js',
+    '@storybook/api': '@chialab/storybook-prebuilt/api',
+    '@storybook/addons': '@chialab/storybook-prebuilt/addons',
+    '@storybook/channel-postmessage': '@chialab/storybook-prebuilt/channel-postmessage',
+    '@storybook/channels': '@chialab/storybook-prebuilt/channels',
+    '@storybook/csf': '@chialab/storybook-prebuilt/csf',
+    '@storybook/client-api': '@chialab/storybook-prebuilt/client-api',
+    '@storybook/client-logger': '@chialab/storybook-prebuilt/client-logger',
+    '@storybook/core-events': '@chialab/storybook-prebuilt/core-events',
+    '@storybook/preview-web': '@chialab/storybook-prebuilt/preview-web',
+    '@storybook/components': '@chialab/storybook-prebuilt/components',
+    '@storybook/router': '@chialab/storybook-prebuilt/router',
+    '@storybook/semver': '@chialab/storybook-prebuilt/semver',
+    '@storybook/store': '@chialab/storybook-prebuilt/store',
+    '@storybook/theming': '@chialab/storybook-prebuilt/theming',
+    '@storybook/addon-docs': '@chialab/storybook-prebuilt/docs',
+    '@storybook/addon-docs/blocks': '@chialab/storybook-prebuilt/docs',
+    '@storybook/addon-essentials/register': '@chialab/storybook-prebuilt/essentials/register',
+    '@storybook/addon-essentials': '@chialab/storybook-prebuilt/essentials',
+    '@storybook/addon-storysource/register': '@chialab/storybook-prebuilt/storysource/register',
+    'storybook-design-token': '@chialab/storybook-prebuilt/storybook-design-token',
+    'storybook-design-token/dist/doc-blocks.js': '@chialab/storybook-prebuilt/storybook-design-token',
+    'storybook-design-token/register': '@chialab/storybook-prebuilt/storybook-design-token/register',
+};
